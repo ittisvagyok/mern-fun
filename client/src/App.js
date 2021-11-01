969f36b741fbedde4ec44c3fda812f54055bf93b
ittisvagyok <ittisvagyok@gmail.com>
2021-11-01 16:51:38 +0100

    MERN works

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
 
 
   callApi() {
-    fetch("http://localhost:9000/testAPI")
+    fetch("http://localhost:5000/testAPI")
       .then(res => res.text())
       .then(res => this.setState({ apiResponse: res }))
       .catch(err => err);
--- server/app.js
+++ server/app.js
@@ -16,6 +16,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(cors());
+app.options('*', cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
