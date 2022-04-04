8c6b02971bac4bb92624f46548b9e8760ecc5c06
ittisvagyok <ittisvagyok@gmail.com>
2022-04-05 00:47:04 +0200

    String -> Array -> Display in table

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,20 @@
 import React, { Component } from "react";
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/css/bootstrap-grid.css';
+import '../node_modules/bootstrap/dist/css/bootstrap-reboot.css';
+import Button from 'react-bootstrap/Button';
+import ExpenseTable from './components/ExpenseTable'
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {apiResponse: ""};
+    this.state = {apiResponse: '[]' };    
   }
 
-
   callApi() {
-    fetch("http://localhost:5000/testAPI")
+    fetch("http://localhost:5000/expense")
       .then(res => res.text())
       .then(res => this.setState({ apiResponse: res }))
       .catch(err => err);
@@ -24,21 +28,18 @@ class App extends Component {
   render() {
 
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />          
-          <h1 className="App-intro">
-            {this.state.apiResponse}            
-          </h1>       
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>          
-        </header>
+      <div className="container-fluid">
+        <div className="App">
+          <header className="App-header">
+            {/*<img src={logo} className="App-logo" alt="logo" />*/}
+            <h1>Expense table</h1>
+          </header>
+          <div className="content">
+            <ExpenseTable expensesData={this.state.apiResponse} />
+          </div>
+            <Button variant="success" className="btn-lg">kljsad</Button>{' '}
+          
+        </div>
       </div>
     );
   }
---
+++ client/src/components/ExpenseTable.js
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import Table from 'react-bootstrap/Table';
+
+class ExpenseTable extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  resToArray (data) {
+    let newArray = JSON.parse(data);    
+    return(newArray);
+  }
+
+  render () {
+      //console.log(this.resToArray(this.props.expensesData));      
+      return(
+        <Table hover responsive>
+          <thead>
+            <tr>
+              <th>#
+              </th>
+              <th>Title
+              </th>
+              <th>Ammount
+              </th>
+              <th>Category
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.resToArray(this.props.expensesData).map((element, index) => {                
+              console.log(element);
+              return(
+                <tr key={element._id}>
+                  <td>{index + 1}</td>
+                  <td>{element.title}</td>
+                  <td>{element.ammount}</td>
+                  <td>{element.category}</td>
+                </tr>
+              );
+            })}            
+          </tbody>
+        </Table>
+      );
+  }
+}
+
+export default ExpenseTable;
